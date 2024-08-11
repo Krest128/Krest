257c1a802382042c132d669928e02fc2837d432e
student <student@SlovakstudyUbuntu.myguest.virtualbox.org>
2024-08-11 11:11:42 +0200

    napisal dve funkcii

--- hangman.c
+++ hangman.c
@@ -2,6 +2,42 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include "hangman.h"
+#include <stdbool.h>
+#include <ctype.h>
+
+void is_word_guessed(const char secret[], const char letters_guessed[]){
+    for(int i = 0;i < strlen(secret);i++){
+        bool found = 0;
+        for(int j = 0;j < strlen(letters_guessed);j++){
+            if(tolower(secret) == letters_guessed){
+                found = 1;
+                break;
+            }
+        }
+        if(!found){
+            return 0;
+        }
+    }
+    return 1;
+}
+
+void get_guessed_word(const char secret[], const char letters_guessed[], char guessed_word[]){
+    for(int i = 0; i < strlen(secret);i++){
+        for(int j = 0;j < strlen(letters_guessed);i++){
+            if(tolower(secret) == letters_guessed){
+                guessed_word[i] = secret;        
+            }
+            else {
+                guessed_word[i] = '_';
+            }
+        }
+
+    }
+}
+void get_available_letters(const char letters_guessed[], char available_letters[]){
+    char alfavit[] = "abcdefghijklmnopqrstuvwxyz"
+    for(int i = 0;i < strlen(letters_guessed;))
+}
 
 
 int get_word(char secret[]){
---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "hangman.h"
+
+int is_word_guessed(const char secret[], const char letters_guessed[])
+
+int main(){
+
+
+}
